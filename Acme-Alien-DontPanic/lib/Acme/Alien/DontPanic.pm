75937a69eb4435e0f31e9b2eb7f66fca2a97143b
Graham Ollis <perl@wdlabs.com>
2015-02-06 12:29:01 -0500

    version 0.012

--- Acme-Alien-DontPanic/lib/Acme/Alien/DontPanic.pm
+++ Acme-Alien-DontPanic/lib/Acme/Alien/DontPanic.pm
@@ -3,7 +3,7 @@ package Acme::Alien::DontPanic;
 use strict;
 use warnings;
 
-our $VERSION = '0.011';
+our $VERSION = '0.012';
 $VERSION = eval $VERSION;
 
 use parent 'Alien::Base';
--- Acme-Ford-Prefect/lib/Acme/Ford/Prefect.pm
+++ Acme-Ford-Prefect/lib/Acme/Ford/Prefect.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use Acme::Alien::DontPanic;
 
-our $VERSION = '0.011';
+our $VERSION = '0.012';
 require DynaLoader;
 our @ISA = 'DynaLoader';
 __PACKAGE__->bootstrap($VERSION);
